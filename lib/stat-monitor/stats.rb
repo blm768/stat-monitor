69f2753f66fe42b2b6daf56b7ad28e83e05c0377
Ben Merritt <blm768@gmail.com>
2012-08-29 16:03:23 -0700

    Started /proc/cpuinfo parsing

--- lib/stat-monitor/stats.rb
+++ lib/stat-monitor/stats.rb
@@ -1,10 +1,23 @@
 module Stats
   class LocalStats
     def self.get()
-     cpuData = File.open("/proc/cpuinfo", "r") 
-     cpuData.each_line do |line|
+      data = {}
 
-     end
+      #Load processor data
+      data['processors'] = []
+      cpuData = File.open("/proc/cpuinfo", "r") 
+      cpuData.each_line do |line|
+        puts line
+        line.chomp!
+        case line
+          when /^processor\s*/
+            begin
+              puts Integer(/\d+/.match(line).captures[0]).to_s
+            recover
+              #
+            end
+        end
+      end
     end
   end
 end
