9f1c22ef07fc63707c492545adbeb2a0e1f00dd6
Ben Merritt <blm768@gmail.com>
2012-09-04 14:37:18 -0700

    Full memory stats; refactoring; more use of /proc

--- lib/stat-monitor/stats.rb
+++ lib/stat-monitor/stats.rb
@@ -1,94 +1,82 @@
 module Stats
   class LocalStats
 
-    def self.memStats()
-    memTotal = nil
-    memFree = nil
-    swapTotal = nil
-    swapFree = nil
-    memCached = nil
-    swapCached = nil
-    
-    File.open("/proc/meminfo") do |file|
-      def getValue(line)
-        Integer(/\d+/.match(line)[0])
-      end
-      file.each_line do |line|
-        case line
-          when /^MemTotal:\s/
-            memTotal = getValue(line)
-          when /^MemFree:\s/
-            memFree = getValue(line)
-          when /^SwapTotal:\s/
-            swapTotal = getValue(line)
-          when /^SwapFree:\s/
-            swapFree = /\d+/.match(line)[0]
-          when /^Cached\s/
-            memCached = /\d+/.match(line[0])
-          when /^SwapCached\s/
-            swapCached = /\d+/.match(line[0])
-        end
-      end
+    def self.getValue(line)
+      Integer(/\d+/.match(line)[0])
     end
 
-    if memFree
-      if memTotal
-        memFree /= memTotal
-      else
-        memFree = nil
-      end
-    end
+    private_class_method :getValue
 
-    if swapFree
-      if swapTotal
-        swapFree /= swapTotal
-      else
-        swapFree = nil
-      end
-    end
-
-    {'Total' => memTotal, 'Free' => memFree, 'SwapTotal' => swapTotal, 'SwapFree' => swapFree, 'Cached' => memCached, 'SwapCached' => swapCached}
-  end
-
-  def self.get()
-      data = {}
-
-      #Load processor data
-
-      numProcessors = 0
+    def self.numProcessors()
+      num = 0
 
       cpuData = File.open("/proc/cpuinfo", "r") 
       cpuData.each_line do |line|
         line.chomp!
-        
-        numProcessors += 1 if /^processor\s*:\s*\d$/ =~ line
+        num += 1 if /^processor\s*:\s*\d$/ =~ line
       end
       
       cpuData.close
-      
-      users = {}
-
-      #Get user data
-      userData = `w`
-      #Remove first two lines, which are just column headers.
-      userData = userData.to_a[2 .. -1].join
-
-      userData.each_line do |line|
-        fields = line.split(/\s+/)
-        userName = fields[0]
-        users[userName] = {} unless users.include?(userName)
-      end
-      
-      #Return data
-      data['processorCount'] = numProcessors
-     # data['cpuLoad'] = cpuLoad
-      data['users'] = users
-      #data['loadStatus'] = loads
-      data['memory'] = memStats()
-
-      data
+      num
     end
-  end
+
+    def self.loadStats()
+      loadavg = File.open('/proc/loadavg')
+      loads = loadavg.readline.split(' ')[0 ... 3]
+      loadavg.close
+      loads
+    end
+
+    def self.memStats()
+      memTotal = nil
+      memFree = nil
+      swapTotal = nil
+      swapFree = nil
+      memCached = nil
+      swapCached = nil
+      
+      File.open("/proc/meminfo") do |file|
+        file.each_line do |line|
+          case line
+            when /^MemTotal:\s/
+              memTotal = getValue(line)
+            when /^MemFree:\s/
+              memFree = getValue(line)
+            when /^SwapTotal:\s/
+              swapTotal = getValue(line)
+            when /^SwapFree:\s/
+              swapFree = getValue(line)
+            when /^Cached:\s/
+              memCached = getValue(line)
+            when /^SwapCached:\s/
+              swapCached = getValue(line)
+          end
+        end
+      end
+
+      if memFree
+          if memTotal
+          memFree /= memTotal
+        else
+          memFree = nil
+        end
+      end
+
+      if swapFree
+        if swapTotal
+          swapFree /= swapTotal
+        else
+          swapFree = nil
+        end
+      end
+
+      {'Total' => memTotal, 'Free' => memFree, 'SwapTotal' => swapTotal, 'SwapFree' => swapFree, 'Cached' => memCached, 'SwapCached' => swapCached}
+    end
+
+    def self.get()
+        {'Processors' => numProcessors, 'Memory' => memStats, 'Load' => loadStats}
+    end
 
   end
+end
 
